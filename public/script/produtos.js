3766b4aa796b520e35835eb180a4068fa7d33009
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-27 14:49:30 -0300

    adicionando botao

--- public/script/produtos.js
+++ public/script/produtos.js
@@ -18,7 +18,7 @@ http.onload = function () {
                     <div class="card-body">
                         <h5 class="card-title">${item.title}</h5>
                         <p class="card-text">${item.description}</p>
-                        <a href="#" class="btn btn-primary" value="${item.id}" >Detalhes</a>
+                        <button href="#" class="btn btn-primary" value="${item.id}" >Detalhes</button>
                     </div>
                 </div>
             </div>
