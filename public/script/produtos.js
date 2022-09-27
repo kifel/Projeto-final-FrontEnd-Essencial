4260f9c7d379785927c1fc472fcfedb2e577b358
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-27 14:44:22 -0300

    ler aquivos no json e colocar na tela

---
+++ public/script/produtos.js
@@ -0,0 +1,29 @@
+let http = new XMLHttpRequest();
+
+http.open("get", "../database/produtos.json", true);
+
+http.send();
+
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let produtos = JSON.parse(this.responseText);
+
+    let output = "";
+
+    for (let item of produtos) {
+      output += `
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="${item.image}" alt="${item.image} class="card-img-top" >
+                    <div class="card-body">
+                        <h5 class="card-title">${item.title}</h5>
+                        <p class="card-text">${item.description}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    document.querySelector(".produtos").innerHTML = output;
+  }
+};
