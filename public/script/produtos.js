7fc610d7a1ec6cf95323b68bfc9282a9d3b26903
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-27 14:47:10 -0300

    adicionando id

--- public/script/produtos.js
+++ public/script/produtos.js
@@ -18,7 +18,7 @@ http.onload = function () {
                     <div class="card-body">
                         <h5 class="card-title">${item.title}</h5>
                         <p class="card-text">${item.description}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn btn-primary" value="${item.id}" >Detalhes</a>
                     </div>
                 </div>
             </div>
