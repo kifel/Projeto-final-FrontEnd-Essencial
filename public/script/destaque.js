6917c38c82d40b3510216f03a295875093de1d4e
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-27 21:46:44 -0300

    finalizando produtos na tela

--- public/script/destaque.js
+++ public/script/destaque.js
@@ -12,7 +12,7 @@ fetch("../../src/database/produtos.json")
     destaque.forEach((item) => {
       out += `
             <div class="col-12 col-md-6 col-lg-3">
-                <div class="card">
+                <div class="card card-produto">
                     <img src="${item.image}" alt="${item.image} class="card-img-top" height="300" >
                     <div class="card-body">
                         <h5 class="card-title">${item.title}</h5>
