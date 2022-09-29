8577cd14ef52bbd16970e11cb922380b5fa959d1
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-29 14:25:20 -0300

    darkMode index.html

--- public/script/main.js
+++ public/script/main.js
@@ -3,3 +3,24 @@ function show(id) {
 
   localStorage.setItem("id", id);
 }
+
+function darkMode() {
+  const buttonL = document.querySelector(".bi-moon-fill");
+  const buttonS = document.querySelector(".bi-brightness-high-fill");
+
+  if (buttonL) {
+    buttonL.classList.remove("bi-moon-fill");
+    buttonL.classList.add("bi-brightness-high-fill");
+  }
+
+  if (buttonS) {
+    buttonS.classList.remove("bi-brightness-high-fill");
+    buttonS.classList.add("bi-moon-fill");
+  }
+
+  document.documentElement.classList.toggle("dark-mode");
+
+  document
+    .querySelectorAll(".inverted")
+    .forEach((el) => el.classList.toggle("invert"));
+}
