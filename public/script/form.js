335ec171579b4dda21f8afd5ac2eb9aa4fd85b29
João Victor de Mello Pereira <joaovictorm.pessoal@gmail.com>
2022-09-29 15:41:23 -0300

    Amanda (#21)
    * salvando em LS
    
    * ajuste form
    
    Co-authored-by: AmandaCasadem <amanda12kcb@gmail.com>

--- public/script/form.js
+++ public/script/form.js
@@ -17,14 +17,13 @@ document.getElementById("botao").addEventListener("click",(e)=>{
    }
   if(cont!= 3){
     e.preventDefault
-    alert ("Email deve conter "@" e "."
-     e a mensagem no minimo 5 caracter" )
+    alert ("Email deve conter '@' e '.' e a mensagem no minimo 5 caracter" )
   }
   else{
     localStorage.nomeform=nomeform
     localStorage.emailform= emailform
     localStorage.mensagemform= mensagemform
-  document.getElementById("idnome").value=""  
+  document.getElementById("idnome").value=""
   document.getElementById("idemail").value="" 
   document.getElementById("idmensagem").value="" 
 }
